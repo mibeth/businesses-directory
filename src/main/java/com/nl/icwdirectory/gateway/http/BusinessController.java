a5f04e2574afbb2cb51d7ee54016d3b17e285607
Mike <hollowlife7@gmail.com>
2021-01-12 18:17:49 +0100

    Unused import line removed

--- src/main/java/com/nl/icwdirectory/gateway/http/BusinessController.java
+++ src/main/java/com/nl/icwdirectory/gateway/http/BusinessController.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toUnmodifiableList;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
